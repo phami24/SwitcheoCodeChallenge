c98688813ddbfd77059c4bfed02acfe2f1587bdd
Minh Pham <buibinhminhpham2004@gmail.com>
2024-07-10 15:43:21 +0700

    config route

--- Problem2/swap-ui/src/App.js
+++ Problem2/swap-ui/src/App.js
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SwapPage from "./pages/SwapPage";
 import "./App.css";
@@ -6,8 +6,8 @@ import "./App.css";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/swap" element={<SwapPage />} />
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/" element={<SwapPage />} />
     </Routes>
   );
 }
